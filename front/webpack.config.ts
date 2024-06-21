ac94b077f7c803dac97f412d1ea35369385a39e8
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-06-21 20:23:34 +0900

    update(webpack): 용량증가 설정

--- front/webpack.config.ts
+++ front/webpack.config.ts
@@ -93,6 +93,11 @@ const config: Configuration = {
     devMiddleware: { publicPath: '/dist/' },
     static: { directory: path.resolve(__dirname, 'public') },
   },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
 };
 
 if (isDevelopment && config.plugins) {
