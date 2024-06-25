d029d69f9c359e7a5689230316066d1384ae4056
kingheedo <52102550+kingheedo@users.noreply.github.com>
2024-06-25 19:37:32 +0900

    fix(docker-compose): https인증 command 수정

--- front/renew_certificates.sh
+++ front/renew_certificates.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
-# Function to request initial certificate
-request_initial_certificate() {
-    certbot certonly --webroot -w /usr/share/nginx/html \
-    --non-interactive \
-    --agree-tos \
-    --email "$CERTBOT_EMAIL" \
-    -d "$CERTBOT_DOMAIN1" \
-    -d "$CERTBOT_DOMAIN2"
-}
-
-# Request initial certificate
-request_initial_certificate
-
-# Function to renew certificates
-renew_certificates() {
-    while :; do
-        certbot renew --nginx --non-interactive
-        sleep 12h
-    done
-}
-
-# Start the renewal process
-renew_certificates
-
+while true; do
+  certbot renew --nginx --quiet
+  sleep 12h
+done
